23704caf03cbb54ac6d5c620064d910390ea1abc
Edgar Velazquez <edgarvl@velmen.com>
2017-12-07 22:26:54 -0600

    Bootstrapping the application with some initial values

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+        //Eric
+        Author eric = new Author("Eric","Evans");
+        Book ddd =new Book("Domain Driven Design","1234","Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        //Rod
+        Author rod = new Author("Rod", "Thompson");
+        Book noEjb = new Book("J2EE Development without EJB", "23444","Worx");
+        rod.getBooks().add(noEjb);
+    }
+}
