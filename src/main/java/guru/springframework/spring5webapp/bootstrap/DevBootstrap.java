1643fcee7aeff13e212d626c7d2d143164662944
Edgar Velazquez <edgarvl@velmen.com>
2017-12-07 22:37:51 -0600

    Bootstrapping the application with some initial values

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,10 +2,22 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -19,9 +31,15 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
         //Rod
         Author rod = new Author("Rod", "Thompson");
         Book noEjb = new Book("J2EE Development without EJB", "23444","Worx");
         rod.getBooks().add(noEjb);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEjb);
     }
 }
